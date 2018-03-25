486e2aa3fa83c8ea2fb98abf1d23d2bcf19e57c5
A64Danil <a64danil@mail.ru>
2018-03-25 17:18:50 +0300

    Remove unused lines, separate functions, optimised

--- dist/bcte.js
+++ dist/bcte.js
@@ -1,25 +1,19 @@
 /**
  * Created by Danil on 24.03.2018.
- *
- *
- * TODO: вынести сет куки гет куки
- * TODO: написать проверку на stayInTime, и если она число от 1 до ..., то не запускать функцию
- * TODO: УДАЛИТЬ лишние комментарии
- *
- *
+ * On Close Tab Event
+ * Version 0.9.0
  */
+
 
 // Restrict globals by IIFE
 (function () {
 
-
-    var cookieName = 'QoobeoCook1'; // Set name of your cookie
+    var cookieName = 'QoobeoLastTest'; // Set name of your cookie
     var cookieNameValBefore = 'unvisited'; // Set first value of your cookie
     var cookieNameValAfter = 'visited'; //  Set second value of your cookie
-    var liveTime = 2; // numeric - days
-    var stayInTime = 4; // numeric - seconds
-    // TODO: Создать переменную, которая будет тру или фалс, и если фалс то не создаём функцию проверки по таймеру
-    var targetURL = '/bcte/demo/test2.html'; // url to your targeted page
+    var liveTime = 0.1; // numeric - days
+    var stayInTime = 10; // numeric - seconds OR boolean - false
+    var targetURL = '/bcte/demo/visit_this.html'; // url to your targeted page
 
     // x * 1000 - a second
     // x * 60 * 1000 - a minute
@@ -27,162 +21,138 @@
     // x * 24 * 60 * 60 * 1000 - a day
     // x * 7 * 24 * 60 * 60 * 1000 - a week
 
-
     var d = new Date(); // Get new current date
+
 
     //var loc = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds());
 
     //alert(d.getTime());
     //alert(d.getTimezoneOffset() * 60 * 1000);
 
-    function setCookie(name, value, expires, path, domain, secure)
-    {
-        document.cookie =    name + "=" + escape(value) +
-            ((expires) ? "; expires=" + (new Date(expires)) : "") +
-            ((path) ? "; path=" + path : "") +
-            ((domain) ? "; domain=" + domain : "") +
-            ((secure) ? "; secure" : "");
-    }
-
-
-    /**
-     * Получить значение куки по имени name
-     *
-     */
-    function getcookie(name)
-    {
-        var cookie = " " + document.cookie;
-        var search = " " + name + "=";
-        var setStr = null;
-        var offset = 0;
-        var end = 0;
-
-        if (cookie.length > 0)
-        {
-            offset = cookie.indexOf(search);
-
-            if (offset != -1)
-            {
-                offset += search.length;
-                end = cookie.indexOf(";", offset)
-
-                if (end == -1)
-                {
-                    end = cookie.length;
-                }
-
-                setStr = unescape(cookie.substring(offset, end));
-            }
-        }
-
-        return(setStr);
-    }
-
-    /**
-     * Check existing of the cookie in client PC
-     * TODO: переписать функцию на switch, установить сравниение (getcookie(cookName) === null), в остальных случаях - return
-     */
-    function cookieCheck(cookName = cookieName) {
-        console.log('TIK');
-        if (getcookie(cookName) === cookieNameValAfter) {
-            console.log('Куки ' + cookName + ' установлены, их статус: '+ getcookie(cookName));
-        }
-        else if (getcookie(cookName) === cookieNameValBefore) {
-            console.log('Куки ' + cookName + ' установлены, их статус: '+ getcookie(cookName));
-        }
-        else if (getcookie(cookName) === null) {
-            console.log('Такой куки не существует, или её время жизни истекло (ну или вы не были на странице акции)');
-            clearInterval(cookTimer);
-        }
-        else {
-            throw new Error('Я не знаю что у вас с куки, но вы явно что-то не то мне присылаете')
-        }
-
-    }
-
-    /**
-     * Check cookie every 2 seconds
-     * TODO: поместить рядом с кукиЧек
-     */
-    var cookTimer = setInterval(cookieCheck, 2000);
-
-
-
-    /**
-     * Add listener
-     * TODO: вынести наверх или рядом с виндоуПопАп
-     */
-    document.body.addEventListener('mouseout', windowPopUp);
-
-    /**
-     * Add listener
-     * TODO: Вложить функцию ремувПопАп внуть СетСаймАу
-     */
-    setTimeout(function removePopUp() {
-        console.log("Обработчик убран");
-        document.body.removeEventListener('mouseout', windowPopUp);
-    }, (stayInTime * 1000));
-
-
-
 
     /**
      * Main function of the project.
      * Check our cookie on the match and position of mouse.
-     *
      */
+    document.body.addEventListener('mouseout', windowPopUp);
     function windowPopUp(e) {
-        console.log(document.cookie);
-        // //alert( document.cookie );
-        // if (e.relatedTarget === null && e.pageY < 0 && getcookie(cookieName) === null) {
-        //     alert("Хочешь закрыть?\r\nТогда посети страницу 'Правильные телефоны' ");
-        // }
-
+        //console.log(document.cookie);
         if (e.relatedTarget === null && e.pageY < 0 && (getcookie(cookieName) === cookieNameValBefore || getcookie(cookieName) === null)) {
             document.body.querySelector('#exampleModalLabel').textContent = 'Внимание!';
             document.body.querySelector('.modal-body').innerHTML = 'Хочешь закрыть?\r\nТогда посети страницу "';
-
-
-
             document.body.querySelector('.modal-body').innerHTML += '<a href="' + targetURL + '">' + targetURL + '</a>';
-            document.body.querySelector('.modal-body').innerHTML +=  '" ';
-            //console.log($('.modal-body'));
-            //$('.modal-body').textContent = ;
-
+            document.body.querySelector('.modal-body').innerHTML += '" ';
             $('#exampleModal').modal('show');
-            //alert("Хочешь закрыть?\r\nТогда посети страницу '" + targetURL + "' ");
         }
-
-
-
     }
 
     /**
      * Check url of the current page. You can do something after the check.
-     * TODO: переписать на IIFE
      */
-    function urlCheker() {
-        if ( window.location.pathname == targetURL) {
-
+    (function urlCheker() {
+        if (window.location.pathname == targetURL) {
             if (getcookie(cookieName) === cookieNameValAfter) {
                 alert('У вас уже установлены куки VISITED');
-                //setCookie('yourName', 'DanilTest', (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + 20 * 1000);
+                return;
             }
             else {
                 alert('Вы пришли на нужную страницу, скоро мы установим вам куки на 15 секунд');
-                //setCookie('yourName', 'DanilTest', (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + 15 * 1000);
-                setCookie(cookieName, cookieNameValAfter, (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + (liveTime * 24 * 60 * 60 * 1000) );
+                setCookie(cookieName, cookieNameValAfter, (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + (liveTime * 24 * 60 * 60 * 1000));
             }
-
-
         }
 
         if (getcookie(cookieName) === null) {
             console.log("У вас нет куки, сейчас мы вам пропишем");
-            setCookie(cookieName, cookieNameValBefore, (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + (liveTime * 24 * 60 * 60 * 1000) );
+            setCookie(cookieName, cookieNameValBefore, (new Date).getTime() + (new Date).getTimezoneOffset() * 60 * 1000 + (liveTime * 24 * 60 * 60 * 1000));
+        }
+    })();
+
+
+    /**
+     * StayInTime - function will work next "stayInTime" seconds.
+     */
+    (function stayInTimeCheker() {
+        if (typeof stayInTime == 'number' && stayInTime > 0) {
+            setTimeout(function removePopUp() {
+                console.log('The BCTE-Listener was removed');
+                document.body.removeEventListener('mouseout', windowPopUp);
+            }, (stayInTime * 1000));
+        }
+        else if (typeof stayInTime == 'boolean' && stayInTime == false) {
+            console.log('The BCTE-Listener will not be removed ')
+            return;
+        }
+        else {
+            throw new Error('Data type of *stayInTime* not correct')
+        }
+    })();
+
+    /**
+     * Check cookie every 5 seconds
+     */
+    var cookTimer = setInterval(cookieCheck, 5000);
+
+    /**
+     * Check existing of the cookie in client PC
+     */
+    function cookieCheck(cookName = cookieName) {
+        console.log('TIK');
+        var parsedCookieName = getcookie(cookName);
+        switch (parsedCookieName) {
+            case null:
+                console.log('You cookie is clear. You do not need a timer');
+                clearInterval(cookTimer);
+                break;
+            case cookieNameValAfter:
+                console.log('Cookie ' + cookName + ' was set, they have: ' + parsedCookieName);
+                break;
+            case cookieNameValBefore:
+                console.log('Cookie ' + cookName + ' was set, they have: ' + parsedCookieName);
+                break;
+            default:
+                throw new Error('Я не знаю что у вас с куки, но вы явно что-то не то мне присылаете');
+                break;
         }
     }
-    urlCheker();
+
 
 })();
 
+
+/**
+ * Set and Get a Cookie by the name
+ *
+ */
+
+function setCookie(name, value, expires, path, domain, secure) {
+    document.cookie = name + "=" + escape(value) +
+        ((expires) ? "; expires=" + (new Date(expires)) : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+}
+
+function getcookie(name) {
+    var cookie = " " + document.cookie;
+    var search = " " + name + "=";
+    var setStr = null;
+    var offset = 0;
+    var end = 0;
+
+    if (cookie.length > 0) {
+        offset = cookie.indexOf(search);
+
+        if (offset != -1) {
+            offset += search.length;
+            end = cookie.indexOf(";", offset)
+
+            if (end == -1) {
+                end = cookie.length;
+            }
+
+            setStr = unescape(cookie.substring(offset, end));
+        }
+    }
+
+    return (setStr);
+}
